3185fe7bd436dd265178c5815993ade1dedd735f
Daniel Boros <daniel.boros@gmail.com>
2025-03-20 21:34:20 +0100

    add frigate lxc

--- install/frigate-install.sh
+++ install/frigate-install.sh
@@ -3,8 +3,7 @@
 # Copyright (c) 2021-2024 tteck
 # Author: tteck (tteckster)
 # Co-Author: remz1337
-# License: MIT
-# https://github.com/tteck/Proxmox/raw/main/LICENSE
+# Co-Author: mrboros
 
 source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
 color
